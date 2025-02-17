ef65b0e014ab6bea3552e49eced6293b15ca2cd9
Henrique J. Araujo <liderhenrique@gmail.com>
2025-02-17 20:25:31 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-black text-white text-4xl font-bold">{children}</body>
     </html>
   );
 }

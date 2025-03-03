f37fc8d98440f74a1747e7a44f633b5127bb365d
Henrique J. Araujo <liderhenrique@gmail.com>
2025-03-03 07:12:58 -0300

    construindo interface de racking do evento

--- src/componets/icon-button.tsx
+++ src/componets/icon-button.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import React, { ComponentProps } from "react";
+import { twMerge } from 'tailwind-merge'
 
-interface IconButtonProps extends ComponentProps<"button"> {}
+interface IconButtonProps extends ComponentProps<"button"> { }
 
-export default function IconButton(props: IconButtonProps) {
+export default function IconButton({ className, ...props }: IconButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge("p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900", className)}
       {...props}
     />
   );
